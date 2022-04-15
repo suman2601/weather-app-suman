dea892549c17de6bfee27f362e961f269623e470
suman2601 <68642239+suman2601@users.noreply.github.com>
2022-04-15 17:41:20 +0530

    update

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"sumanvadhel26@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
